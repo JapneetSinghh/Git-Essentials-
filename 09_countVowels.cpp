dd3baafc30a6dfef5f96bc27cd70f16745a325a8
Japneetsinghh <japneet8208@gmail.com>
2025-02-24 13:48:53 -0500

    Trying to cause a merge conflict

--- 09_countVowels.cpp
+++ 09_countVowels.cpp
@@ -4,13 +4,14 @@
 using namespace std;
 char vowels[10] = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
 
-bool checkVowel(char ch)
+// I am modifing this file, trying to cause a merge conflict
+bool letsCheckVowels(char ch)
 {
     int n = sizeof(vowels) / sizeof(vowels[0]);
 
     for (int i = 0; i <= n; i++)
     {
-        if (ch == vowels[i])
+        if (vowels[i]==ch)
         {
             return true;
         }
@@ -25,11 +26,10 @@ int main()
     string str = "My name is Sidak";
     for (int i = 0; str[i] != '\0'; i++)
     {
-        if (checkVowel(str[i]))
+        if (letsCheckVowels(str[i]))
         {
-            isVowel++;
+            isVowel = isVowel + 1;
         }
     }
-
     cout<<"Number of vowels are: "<< isVowel<<endl;
 }
